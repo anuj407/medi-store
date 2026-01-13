4a5360998f6f03eca73fb7cdf85283bf4a5edc4a
anuj407 <anuj97953a@gmail.com>
2026-01-13 08:20:01 +0530

    feat: add server listening functionality with dynamic port configuration

--- Server/src/index.js
+++ Server/src/index.js
@@ -7,5 +7,9 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 export default app;
